86db9a3e1ebb98f604d9bd42904eb69aa55e8a08
Avz20 <vz.andrea20@gmail.com>
2024-06-14 14:24:06 +0200

    Cambios

--- src/app/components/pages/foro/entrada-foro/entrada-foro.component.ts
+++ src/app/components/pages/foro/entrada-foro/entrada-foro.component.ts
@@ -3,111 +3,113 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EntradasService } from '../../../../services/entradas/entradas.service';
-// import { FooterLayoutComponent } from '../../layout/footer/footer.component';
-// import { HeaderLayoutComponent } from '../../layout/header/header.component';
 
 @Component({
   selector: 'app-entrada-foro',
   standalone: true,
-  imports: [ FormsModule, CommonModule ], // HeaderLayoutComponent, FooterLayoutComponent
+  imports: [FormsModule, CommonModule], // HeaderLayoutComponent, FooterLayoutComponent
   templateUrl: './entrada-foro.component.html',
-  styleUrl: './entrada-foro.component.css'
+  styleUrls: ['./entrada-foro.component.css']
 })
-
 export class EntradaForoComponent implements OnInit {
 
-  // Crear Entrada
-  entradaForo = { nombre: ' ', titulo: ' ', texto_comentario: ' ', ID_autor: null, ID_entrada: null };
-  constructor(private entradasService: EntradasService,
-              private route: ActivatedRoute,
+  entradaForo: any = { titulo: '', textoComentario: '', autor: '', ID_entrada: '', fecha: '', hora: '' };
+  // currentUserId: number;
+
+  constructor(private entradasService: EntradasService, 
+              private route: ActivatedRoute, 
               private router: Router) {}
 
   createEntradaForo(entryForm: NgForm): void {
     if (entryForm.invalid) {
       alert('Por favor, llena todos los campos');
-      return
+      return;
     }
+
+    const currentDate = new Date();
+    this.entradaForo.fecha = currentDate.toISOString().split('T')[0];
+    this.entradaForo.hora = currentDate.toTimeString().split(' ')[0];
+
     this.entradasService.createEntradaForo(this.entradaForo).subscribe({
       next: (data) => {
-      console.log('Entry created succesfully', data);
-      alert(`La entrada con el autor: ${this.entradaForo.nombre}, título: ${this.entradaForo.titulo} y mensaje: ${this.entradaForo.texto_comentario} ha sido creada exitosamente.`);
-      entryForm.resetForm();
-    },
-    error: (error) => {
-      console.error('Error creating entry', error);
-    },
+        console.log('Entry created successfully', data);
+        alert(`La entrada con el título: ${this.entradaForo.titulo} y mensaje: ${this.entradaForo.textoComentario} ha sido creada exitosamente.`);
+        entryForm.resetForm();
+        this.loadEntradas();
+      },
+      error: (error) => {
+        console.error('Error creating entry', error);
+      },
     });
   }
 
-  // Ver Entrada
   entradasForo: any[] = [];
-  sort: string | null = null;
-  filter: string | null = null;
   id: string | null = null;
 
   ngOnInit(): void {
-    const idQuery = this.route.snapshot.queryParamMap.get('idQuery');
-    const sort = this.route.snapshot.queryParamMap.get('sort');
-    const filter = this.route.snapshot.queryParamMap.get('filter');
     this.id = this.route.snapshot.paramMap.get('id');
     console.log('ID', this.id);
     this.loadEntradas();
+    // this.currentUserId = this.entradasService.getCurrentUserId();
   }
+
   loadEntradas(): void {
     this.entradasService.getEntradasForo().subscribe({
-      next: (data) => {
-        this.entradasForo = data.data;
+      next: (data: any) => {
+        console.log('Data received from backend:', data);
+        this.entradasForo = data;
         console.log('Entradas: ', this.entradasForo);
       },
-      error: (error) => {
+      error: (error: any) => {
         console.error('There was an error: ', error);
       },
     });
   }
+
   onRefreshClick(): void {
     this.loadEntradas();
   }
 
-  // Actualizar y editar Entrada
   updateEntrada() {
     if (!this.entradaForo.ID_entrada) {
-      alert('Por favor, selecciona un entrada');
+      alert('Por favor, selecciona un id.');
       return;
     }
-    if (
-      !this.entradaForo.nombre &&
-      !this.entradaForo.titulo &&
-      !this.entradaForo.texto_comentario
-    ) {
-      alert('Por favor, selecciona al menos un campo para editar');
+    if (!this.entradaForo.titulo && !this.entradaForo.textoComentario) {
+      alert('Por favor, selecciona al menos un campo para actualizar.');
       return;
     }
-    const updatedEntradaForo: any = {...this.entradaForo };
-    for (const key in this.entradasForo) {
-      if (this.entradasForo[key]) {
-        console.log('key', key);
-        console.log('this.entradaForo[key]', this.entradasForo[key]);
-        updatedEntradaForo[key] = this.entradasForo[key];
-      } else {
-        console.log('key', key);
-        console.log('this.entradaForo[key]', this.entradasForo[key]);
+    const updatedEntradaForo: any = {};
+    for (const key in this.entradaForo) {
+      if (this.entradaForo[key]) {
+        updatedEntradaForo[key] = this.entradaForo[key];
       }
     }
-    console.log('updatedEntradaForo', updatedEntradaForo);
-    this.entradasService.updateEntradaForo(updatedEntradaForo).subscribe ({
-      next: (data) => {
-        console.log('Entry succesfully updated', data);
-        alert(`La entrada con id: ${this.entradaForo.ID_entrada} ha sido actualizada.`);
+    this.entradasService.updateEntradaForo(updatedEntradaForo).subscribe({
+      next: (data: any) => {
+        console.log('Entry updated successfully', data);
+        alert(`La Entrada con id:${this.entradaForo.ID_entrada} ha sido actualizado exitosamente.`);
         this.loadEntradas();
       },
-      error: (error) => {
+      error: (error: any) => {
         console.error('Error updating entry', error);
       },
-    })
-
-    // this.router.navigate(['updateEntradaForo', entradaForo.ID_entrada]);
-
+    });
   }
 
-  // Eliminar Entrada
+  deleteEntrada(id: number): void {
+    // console.log('Deleting entry with id:', id); // Debugging line
+    // this.entradasService.deleteEntradaForo(id).subscribe({
+    //   next: (data: any) => {
+    //     console.log('Evento eliminado con éxito', data);
+    //     this.loadEntradas();
+    //   },
+    //   error: (error: any) => {
+    //     console.error('Error al eliminar el evento', error);
+    //   },
+    // });
+    this.entradasService.deleteEntradaForo(id).subscribe(() => {
+      this.entradaForo = this.entradaForo.filter(this.entradaForo , this.entradaForo.id !== id);
+  });
+  }
 }
--- src/app/services/entradas/entradas.service.ts
+++ src/app/services/entradas/entradas.service.ts
@@ -6,23 +6,53 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 
-export class EntradasService {
-  private apiUrl = 'http://localhost:8080/api/v1/entradasForo';
+// export class EntradasService {
+//   private apiUrl = 'http://localhost:8081/api/v1/entradasForo';
 
+//   private http = inject(HttpClient);
+//   constructor() {}
+
+//   getEntradasForo(): Observable<any> {
+//     return this.http.get(this.apiUrl);
+//   }
+//   createEntradaForo(entradaForo: any): Observable<any> {
+//     return this.http.post(this.apiUrl, entradaForo);
+//   }
+//   updateEntradaForo(entradaForo: any): Observable<any> {
+//     return this.http.put(`${this.apiUrl}`, entradaForo);
+//   }
+//   deleteEntradaForo(entradaForo: any): Observable<any> {
+//     console.log('Eliminando entrada', entradaForo);
+//     return this.http.delete(`${this.apiUrl}`, { body: entradaForo });
+//   }
+// }
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable, inject } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+export class EntradasService {
+  private apiUrl = 'http://localhost:8081/api/v1/entradasForo';
   private http = inject(HttpClient);
+
   constructor() {}
 
   getEntradasForo(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
+
   createEntradaForo(entradaForo: any): Observable<any> {
     return this.http.post(this.apiUrl, entradaForo);
   }
-  updateEntradaForo(entradaForo: any): Observable<any> {
+
+  updateEntradaForo( entradaForo: any): Observable<any> {
     return this.http.put(`${this.apiUrl}`, entradaForo);
   }
-  deleteEntradaForo(entradaForo: any): Observable<any> {
-    console.log('Eliminando entrada', entradaForo);
-    return this.http.delete(`${this.apiUrl}`, { body: entradaForo });
+
+  deleteEntradaForo(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
+
